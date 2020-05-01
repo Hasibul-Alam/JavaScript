c64772819eb82291cac808e6bf1408e680800314
Hasibul-Alam <halambd7@gmail.com>
2020-05-01 12:47:58 +0600

    First commit

---
+++ js/youtube.js
@@ -0,0 +1,8 @@
+/*document.getElementById("button").onclick = function(){
+    document.getElementById("confirm").innerHTML = "Order placed. Check email for confirmation.";
+    document.getElementById("button").style.display = "none";
+}*/
+
+var username = prompt();
+alert(username);
+//console.log(username);

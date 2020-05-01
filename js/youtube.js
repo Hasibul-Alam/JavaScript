4f7afc92e3afc104d4b0405803d7ffd451b681d3
Hasibul-Alam <halambd7@gmail.com>
2020-05-01 22:05:26 +0600

    Second Commit

--- js/youtube.js
+++ js/youtube.js
@@ -3,6 +3,67 @@
     document.getElementById("button").style.display = "none";
 }*/
 
-var username = prompt();
-alert(username);
+/*var username = prompt();
+alert(username);*/
 //console.log(username);
+
+/*{
+    let x = 5;
+    let b = '10';
+
+    x = x + b;
+    console.log(x);
+}*/
+
+// Object variable
+
+/*let person = {
+    name: "Hasib",
+    age: 26,
+    favFood: 'Pizza --_--',
+    fun: function(){
+        console.log('yah!');
+    }
+}
+
+//person.fun();*/
+
+/*{
+    let myName = 'Hasib';
+    console.log(myName.toUpperCase());
+}
+
+{
+    console.log(Number.MAX_SAFE_INTEGER);
+    let x = 9007199254740992;
+    console.log(Number.isSafeInteger(x));
+    console.log(Math.pow(x,200));;
+
+}*/
+
+/*{
+    let input = prompt('Enter a binary number:');
+    console.log(typeof(input));
+    console.log(input + ' in decimal',Number.parseInt(input,10));
+    console.log(input + ' in binary',Number.parseInt(input,2));
+    console.log(input + ' in octal',Number.parseInt(input,8));
+    console.log(input + ' in Hex',Number.parseInt(input,16));
+}
+{
+    let input = Number(prompt('Put in a decimal number:'));
+    console.log(input + ' in decimal to decimal: '+ input);
+    console.log(input + ' in decimal to binary: '+ input.toString(2));
+    console.log(input + ' in decimal to octal: '+ input.toString(8));
+    console.log(input + ' in decimal to Hex: '+ input.toString(16));
+}*/
+
+
+{
+    let myName = 'Hasib';
+    console.log(`My name is ${myName}`);
+    let test = 'this is hasi hd uih  o jskdgohuhouid\
+ ldgjdgojkgkjdk lkdfuo l;kjdofioi\
+ dfgkje oi lkjg lk';
+    console.log(test);
+    console.log(test.length);
+}
